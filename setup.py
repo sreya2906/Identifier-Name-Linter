4734b64b9b15105017d1a67c26d815975c01c317
sreya2906 <77694442+sreya2906@users.noreply.github.com>
2021-11-24 18:36:19 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup
-import os
-from git.repo.base import Repo
-
-if not os.path.exists("./Language-Implementations/tree-sitter-python"):
-    Repo.clone_from("https://github.com/tree-sitter/tree-sitter-python", "./Language-Implementations/tree-sitter-python")
-if not os.path.exists("./Language-Implementations/tree-sitter-javascript"):
-    Repo.clone_from("https://github.com/tree-sitter/tree-sitter-javascript", "./Language-Implementations/tree-sitter-javascript")
-if not os.path.exists("./Language-Implementations/tree-sitter-ruby"):
-    Repo.clone_from("https://github.com/tree-sitter/tree-sitter-ruby", "./Language-Implementations/tree-sitter-ruby")
-if not os.path.exists("./Language-Implementations/tree-sitter-go"):
-    Repo.clone_from("https://github.com/tree-sitter/tree-sitter-go", "./Language-Implementations/tree-sitter-go")
 
 setup(name='Identifier-Name-Linter',
       version='1.0',
@@ -20,6 +9,6 @@ setup(name='Identifier-Name-Linter',
       license='MIT',
       python_requires='>3.6',
       install_requires=[
-          'PyGithub', 'tree_sitter', 'pyenchant', 'GitPython'
+          'PyGithub', 'tree_sitter', 'pyenchant'
       ],
       zip_safe=False)
